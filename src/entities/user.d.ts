757f9db87fbc808ce60da4fb1b631acafc719f30
Pradhana Indra <pradhana.indra@gmail.com>
2023-08-02 17:46:05 +0700

    Create user.d.ts

---
+++ src/entities/user.d.ts
@@ -0,0 +1,8 @@
+// Typescript definition of User. The entity can be anything. It does not have to be a Typescript definition. Constans or class is also OK.
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  // ... Other user properties ...
+}
